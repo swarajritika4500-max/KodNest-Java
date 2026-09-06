3ae601aa5d445865aabaef3e6d23b82eb4eff83d
swarajritika4500-max <swarajritika4500@gmail.com>
2026-09-06 10:35:36 +0530

    this is my first update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("Welcome to Java");
+       System.out.println("I am in Java Track");
     }
 }
