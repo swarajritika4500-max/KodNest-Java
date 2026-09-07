ecfce8dee85c1260b05d34ea696056ad6d004420
swarajritika4500-max <swarajritika4500@gmail.com>
2026-09-07 09:36:09 +0530

    this is my third update

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/third.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Display my placement goal.
+        System.out.println("---MY PLACEMENT GOAL---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
